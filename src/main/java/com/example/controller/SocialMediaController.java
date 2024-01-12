4d8cdc6d824d025d824b8469cf51226ae4b458e5
Kevin Geer <kevjgeer@gmail.com>
2024-01-12 00:43:31 +0000

    getAllMessages still 500 error

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,17 @@
 package com.example.controller;
 
+import com.example.entity.Message;
+import com.example.entity.Account;
+import com.example.repository.MessageRepository;
+import com.example.repository.AccountRepository;
+import com.example.service.MessageService;
+import com.example.service.AccountService;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +19,17 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    MessageRepository messageRepository;
+    MessageService messageService;
+    AccountService accountService;
+
+
+    
+    @GetMapping(path = "/messages")
+    public List<Message> getAllMessages(){
+        return messageService.findAllMessages();
+    }
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,25 @@
 package com.example.service;
 
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class MessageService {
+    public MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> findAllMessages(){
+        return messageRepository.findAll();
+    }
 }
